dc1dce7b720d888621b27ede5e447c79349d0563
veeweeherman <vy.hoang.cu@gmail.com>
2015-08-03 15:20:57 -0700

    writing code to put pics onto homepage

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 angular.module('app', [])
-	.controller(cuteCtrl)
-	.factory()
+	.controller('cuteCtrl', function($scope){
+		$scope.imageLocations = [
+			'/img/vy1.jpg',
+			'/img/vy1.jpg'
+		]
+	})
+// insert two images onto page
+// make a vote button for pic 1 or pice 2
+// keep count
+// write to text file the count, so when refreshed it keeps count
