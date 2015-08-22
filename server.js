7e772d2afff422a155438652011677fcc228f691
veeweeherman <vy.hoang.cu@gmail.com>
2015-08-21 22:30:51 -0700

    preparing for deployment

--- server.js
+++ server.js
@@ -1,10 +1,44 @@
+// var express = require('express');
+// var app = express();
+// var path = require('path');
+// // app.get('/', function(req, res){
+// //   res.send('what the heccckkkk');
+// // });
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/index.html'));
+// });
+// app.listen(3000);
+
+
+
+"use strict";
+
 var express = require('express');
+
 var app = express();
 var path = require('path');
-// app.get('/', function(req, res){
-//   res.send('what the heccckkkk');
-// });
+
+
+var port = process.env.PORT || '3000';
+
+//========================================================//
+//   connecting the client and server                     //
+//   allows for CORS (cross origin resource sharing)      //
+//========================================================//
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
-app.listen(3000);
+
+//========================================================//
+//   Calling the server                                   //
+//========================================================//
+var server = app.listen(port, function() {
+  var host = server.address().address;
+  console.log('TinPurr app listening at http://%s:%s -- %s', host, port);
+});
