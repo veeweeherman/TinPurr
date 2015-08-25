41e2c26a56f085a48a41ef3dbff8e193c2c94d37
veeweeherman <vy.hoang.cu@gmail.com>
2015-08-24 17:16:39 -0700

    fixed angular error, on localhost, added static file instruction

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
+app.use(express.static(path.normalize(__dirname + '/')));
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
